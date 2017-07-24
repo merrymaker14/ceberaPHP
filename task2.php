2d28a2ea9e1aff922bb9be2c413cfd505e59773d
Tim Musharapov <tmusharapov@bk.ru>
2017-07-24 12:56:31 +0300

    updating files and 2 tasks

---
+++ task2.php
@@ -0,0 +1,25 @@
+﻿<?php
+
+/**
+  * Получение содержимого папки datafiles в соответствие с регулярным выражением
+  */
+$dir = './datafiles'; 
+$files = scandir($dir);
+$res = array();
+foreach ($files as $file) {
+  if (preg_match('/^[A-Za-z0-9]+.txt$/', $file)) {
+    $res[] = $file;
+  }
+}
+
+/**
+  * Сортировка по имени
+  */
+$res = natcasesort($res)
+
+/**
+  * Вывод на экран содержимого папки datafiles
+  */
+foreach ($res as $r) {
+  echo $r[0]."\n";
+}
